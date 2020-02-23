e8e2f24bbceb1900dfd7864ce7ec4fd9064ffa6f
cb-vinayak <cb-vinayak@cb-vinayak.local>
2020-02-23 21:41:01 +0530

    added utilities to compare

--- src/main/java/Utilities.java
+++ src/main/java/Utilities.java
@@ -2,6 +2,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.GeckoDriverInfo;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import org.openqa.selenium.OutputType;
+import javax.imageio.ImageIO;
+import org.openqa.selenium.TakesScreenshot;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
 
 public class Utilities {
 WebDriver driver;
@@ -12,4 +19,53 @@ public WebDriver fireFox(){
     return driver;
 }
 
+    public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
+
+        //Convert web driver object to TakeScreenshot
+
+        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+
+        //Call getScreenshotAs method to create image file
+
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+
+        //Move image file to new destination
+
+        File DestFile=new File(fileWithPath);
+
+        //Copy file at destination
+
+        FileUtils.copyFile(SrcFile, DestFile);
+
+    }
+    public static boolean compareImage(File fileA, File fileB) {
+        try {
+            // take buffer data from botm image files //
+            BufferedImage biA = ImageIO.read(fileA);
+            DataBuffer dbA = biA.getData().getDataBuffer();
+            int sizeA = dbA.getSize();
+            BufferedImage biB = ImageIO.read(fileB);
+            DataBuffer dbB = biB.getData().getDataBuffer();
+            int sizeB = dbB.getSize();
+            // compare data-buffer objects //
+            if(sizeA == sizeB) {
+                for(int i=0; i<sizeA; i++) {
+                    if(dbA.getElem(i) != dbB.getElem(i)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Failed to compare image files ...");
+            return  false;
+        }
+    }
+
 }
+
+
--- src/test/java/CanvasTest.java
+++ src/test/java/CanvasTest.java
@@ -1,11 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+import java.lang.reflect.AccessibleObject;
 
 public class CanvasTest extends Utilities {
     Utilities utilities = new Utilities();
     @Test
-    public void testCanvas() {
+    public void testCanvas() throws Exception {
         driver = utilities.fireFox();
-        driver.get("https://www.google.com");
+        driver.get("https://www.online-calculator.com/full-screen-calculator/");
+        driver.switchTo().frame("fullframe");
+        WebElement canvasElement = driver.findElement(By.id("animation_container"));
+        Actions action = new Actions(driver);
+        action.moveByOffset(670,254).click().build().perform();
+        this.takeSnapShot(driver, "/Users/cb-vinayak/work/canvastest/test.png") ;
+        Assert.assertTrue(compareImage(new File("/Users/cb-vinayak/work/canvastest/test.png"),new File("/Users/cb-vinayak/work/canvastest/test1.png")));
 
     }
 
